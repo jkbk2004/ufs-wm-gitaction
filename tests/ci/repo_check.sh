7426a833b328b1d9d2b9c3978dd504652821d25c
JONG KIM <jong.kim@noaa.gov>
2024-03-25 20:23:23 -0400

    Update repo_check.sh

--- tests/ci/repo_check.sh
+++ tests/ci/repo_check.sh
@@ -13,8 +13,8 @@ result() {
 }
 
 # Declare variables
-declare -A base fv3 mom6 cice ww3 stoch gocart cmeps cdeps hycom
-submodules="fv3 mom6 cice ww3 stoch gocart cmeps cdeps hycom"
+declare -A base mom6 cice ww3 stoch gocart cmeps cdeps hycom
+submodules="mom6 cice ww3 stoch gocart cmeps cdeps hycom"
 comment=''
 ownerID=$1
 
